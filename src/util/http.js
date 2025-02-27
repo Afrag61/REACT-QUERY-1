a200be6240ce3cd716df5cb9ac4412ea3f1add94
Ahmed Frag <a.frag166@gmail.com>
2025-02-27 05:22:55 +0200

    Updated http.js file

--- src/util/http.js
+++ src/util/http.js
@@ -1,14 +1,15 @@
-export const fetchEvents = async ({signal, searchTerm}) => {
-  let url = "http://192.168.1.3:3000/events";
+export async function fetchEvents({ signal, searchTerm }) {
+  console.log(searchTerm);
+  let url = 'http://localhost:3000/events';
 
-  if(searchTerm){
-    url += "?search=" + searchTerm
+  if (searchTerm) {
+    url += '?search=' + searchTerm;
   }
 
-  const response = await fetch(url, {signal});
+  const response = await fetch(url, { signal: signal });
 
   if (!response.ok) {
-    const error = new Error("An error occurred while fetching the events");
+    const error = new Error('An error occurred while fetching the events');
     error.code = response.status;
     error.info = await response.json();
     throw error;
@@ -17,4 +18,26 @@ export const fetchEvents = async ({signal, searchTerm}) => {
   const { events } = await response.json();
 
   return events;
-};
+}
+
+
+export async function createNewEvent(eventData) {
+  const response = await fetch(`http://localhost:3000/events`, {
+    method: 'POST',
+    body: JSON.stringify(eventData),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (!response.ok) {
+    const error = new Error('An error occurred while creating the event');
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  const { event } = await response.json();
+
+  return event;
+}
