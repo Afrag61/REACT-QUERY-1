98ed746a6ef485c550782471d19c1e6f75e222ed
Ahmed Frag <a.frag166@gmail.com>
2025-02-27 12:14:51 +0200

    Updating Data with Mutations, Optimistic Updating

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -1,9 +1,9 @@
 import { Link, useNavigate, useParams } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 
 import Modal from "../UI/Modal.jsx";
 import EventForm from "./EventForm.jsx";
-import { fetchEvent } from "../../util/http.js";
+import { fetchEvent, updateEvent, queryClient } from "../../util/http.js";
 import LoadingIndicator from "../UI/LoadingIndicator.jsx";
 import ErrorBlock from "../UI/ErrorBlock.jsx";
 
@@ -21,7 +21,32 @@ export default function EditEvent() {
     queryFn: ({ signal }) => fetchEvent({ signal, id }),
   });
 
-  function handleSubmit(formData) {}
+  const { mutate } = useMutation({
+    mutationFn: updateEvent,
+    onMutate: async ({ event }) => {
+      const newEventData = event;
+
+      await queryClient.cancelQueries(["events", id]);
+      const prevEventData = queryClient.getQueryData(["events", id]);
+
+      queryClient.setQueryData(["events", id], newEventData);
+
+      return {
+        prevEventData,
+      };
+    },
+    onError: ({}, {}, context) => {
+      queryClient.setQueryData(["events", id], context.prevEventData);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ["events", id] });
+    },
+  });
+
+  function handleSubmit(formData) {
+    mutate({ event: formData, id });
+    navigate("../");
+  }
 
   function handleClose() {
     navigate("../");
